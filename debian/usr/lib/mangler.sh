977c204fe54aec6e173df02f5713abb71840ce7b
Eric Rand <eric@bijoti.be>
2015-08-30 20:48:59 -0700

    tweaking cut command

--- debian/usr/lib/mangler.sh
+++ debian/usr/lib/mangler.sh
@@ -2,8 +2,8 @@
 # Mangler to take blocklist from basic format to unbound format
 rm /etc/hosts.blacklist
 touch /etc/hosts.blacklist
-DATA=`cut -f 2 /etc/hosts.block`
+DATA=`cut -d " " -f 2 /etc/hosts.block`
 for i in $DATA ;
 do
-	 printf "local-zone: \" %s\" redirect\nlocal-data: \"%s A 127.0.0.1\"\n" $i $i >> /etc/hosts.blacklist
+	 printf "local-zone: \"%s\" redirect\nlocal-data: \"%s A 127.0.0.1\"\n" $i $i >> /etc/hosts.blacklist
 done
