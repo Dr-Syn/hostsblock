f422c19b21b0be27d2e3d836121a2b18dc7b42ab
Eric Rand <eric@bijoti.be>
2015-08-31 00:47:07 -0700

    Changes to mangler to handle bad inputs

--- debian/usr/lib/mangler.sh
+++ debian/usr/lib/mangler.sh
@@ -2,7 +2,8 @@
 # Mangler to take blocklist from basic format to unbound format
 rm /etc/hosts.blacklist
 touch /etc/hosts.blacklist
-DATA=`cut -d " " -f 2 /etc/hosts.block`
+DATA=`cut -d " " -f 2 /etc/hosts.block | tr '[:upper:]' '[:lower:]' | sed -nr '/[.].*[a-zA-Z0-9][a-zA-Z0-9-]+([.][a-zA-Z]{2,15})?$/p' | sort -nr | uniq`
+# Get the second column. Convert to lowercase. Get rid of invalid domains. Sort. Get rid of duplicates.
 for i in $DATA ;
 do
 	 printf "local-zone: \"%s\" redirect\nlocal-data: \"%s A 127.0.0.1\"\n" $i $i >> /etc/hosts.blacklist
